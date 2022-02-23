5873af6987190992c6bd38b8741069e6636af19c
Daffa Z <daffazakyy8@gmail.com>
2022-02-24 01:37:55 +0700

    splitting up view type

--- main.go
+++ main.go
@@ -1,33 +1,21 @@
 package main
 
 import (
-	"html/template"
+	"instaphoto/views"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
-var contactTemplate *template.Template
+var homeTemplate *views.View
+var contactTemplate *views.View
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles(
-		"./views/home.gohtml",
-		"./views/layouts/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
+	homeTemplate = views.NewView("./views/home.gohtml")
+	contactTemplate = views.NewView("./views/contact.gohtml")
 
-	contactTemplate, err = template.ParseFiles(
-		"./views/contact.gohtml",
-		"./views/layouts/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	mux := http.NewServeMux()
+	mux := mux.NewRouter()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/contact", contact)
 	log.Println("Running in port :4000")
@@ -36,14 +24,14 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	if err := homeTemplate.Template.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactTemplate.Execute(w, nil); err != nil {
+	if err := contactTemplate.Template.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
