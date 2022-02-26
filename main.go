be489938736b76f7fd25ffec768aa8da3aad0852
Daffa Z <daffazakyy8@gmail.com>
2022-02-27 02:52:02 +0700

    exercise chapter 6

--- main.go
+++ main.go
@@ -10,29 +10,37 @@ import (
 
 var homeTemplate *views.View
 var contactTemplate *views.View
+var faqTemplate *views.View
 
 func main() {
 	homeTemplate = views.NewView("master", "./views/home.gohtml")
 	contactTemplate = views.NewView("master", "./views/contact.gohtml")
+	faqTemplate = views.NewView("master", "./views/faq.gohtml")
 
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/contact", contact)
+	mux.HandleFunc("/faq", faq)
 	log.Println("Running in port :4000")
 	log.Fatal(http.ListenAndServe(":4000", mux))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := homeTemplate.Template.ExecuteTemplate(w, homeTemplate.Layout, nil)
-	if err != nil {
-		panic(err)
-	}
+	must(homeTemplate.Render(w, nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := contactTemplate.Template.ExecuteTemplate(w, contactTemplate.Layout, nil)
+	must(contactTemplate.Render(w, nil))
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(faqTemplate.Render(w, nil))
+}
+
+func must(err error) {
 	if err != nil {
 		panic(err)
 	}
--- views/view.go
+++ views/view.go
@@ -1,13 +1,19 @@
 package views
 
-import "html/template"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "./views/layouts/"
+	TemplateExt string = ".gohtml"
+)
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"./views/layouts/footer.gohtml",
-		"./views/layouts/navbar.gohtml",
-		"./views/layouts/master.gohtml",
-	)
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -21,4 +27,16 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(fmt.Sprintf("%s/*%s", LayoutDir, TemplateExt))
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
