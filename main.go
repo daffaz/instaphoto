9956fd46c61b62d0593d2fc3eec1b2a73977a75b
Daffa Z <daffazakyy8@gmail.com>
2022-03-01 18:02:58 +0700

    adding sign up form

--- main.go
+++ main.go
@@ -8,36 +8,37 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *views.View
-var contactTemplate *views.View
-var faqTemplate *views.View
+var homeView *views.View
+var contactView *views.View
+var signupView *views.View
 
 func main() {
-	homeTemplate = views.NewView("master", "./views/home.gohtml")
-	contactTemplate = views.NewView("master", "./views/contact.gohtml")
-	faqTemplate = views.NewView("master", "./views/faq.gohtml")
+	homeView = views.NewView("master", "./views/home.gohtml")
+	contactView = views.NewView("master", "./views/contact.gohtml")
+	signupView = views.NewView("master", "./views/signup.gohtml")
 
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/contact", contact)
-	mux.HandleFunc("/faq", faq)
+	mux.HandleFunc("/signup", signup)
+
 	log.Println("Running in port :4000")
 	log.Fatal(http.ListenAndServe(":4000", mux))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	must(homeTemplate.Render(w, nil))
+	must(homeView.Render(w, nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	must(contactTemplate.Render(w, nil))
+	must(contactView.Render(w, nil))
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func signup(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	must(faqTemplate.Render(w, nil))
+	must(signupView.Render(w, nil))
 }
 
 func must(err error) {
