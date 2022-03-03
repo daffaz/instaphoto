3c4b53558c0b5fd3570d2341404bedcd25427e5d
Daffa Z <daffazakyy8@gmail.com>
2022-03-03 17:17:28 +0700

    adding controller and restful view

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"instaphoto/controllers"
 	"instaphoto/views"
 	"log"
 	"net/http"
@@ -8,19 +9,21 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
-var contactView *views.View
-var signupView *views.View
+var (
+	homeView    *views.View
+	contactView *views.View
+)
 
 func main() {
 	homeView = views.NewView("master", "./views/home.gohtml")
 	contactView = views.NewView("master", "./views/contact.gohtml")
-	signupView = views.NewView("master", "./views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	mux := mux.NewRouter()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/contact", contact)
-	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/", home).Methods("GET")
+	mux.HandleFunc("/contact", contact).Methods("GET")
+	mux.HandleFunc("/register", usersC.New).Methods("GET")
+	mux.HandleFunc("/register", usersC.Create).Methods("POST")
 
 	log.Println("Running in port :4000")
 	log.Fatal(http.ListenAndServe(":4000", mux))
@@ -34,11 +37,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(contactView.Render(w, nil))
-}
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
 }
 
 func must(err error) {
